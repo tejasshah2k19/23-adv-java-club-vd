86a279b92f0cabac172f07e5462a73e2d61bdde2
Tejas Shah <tejasshah2k19@gmail.com>
2023-11-21 15:50:02 +0530

    init

---
+++ src/main/java/com/controller/SignupServlet.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet; // jakarta 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//ctrl + shift + o 
+//ctrl +m 
+
+
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet{
+
+	
+	public void service(HttpServletRequest request,HttpServletResponse response) {
+		
+		System.out.println("SignupServlet Called.....");
+		
+		//
+		
+	}
+}
